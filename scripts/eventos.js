0e46488797680feafd160847b750b25b659b4cfe
Pedro Giacciani <91699780+PedroGiacciani@users.noreply.github.com>
2026-09-05 13:45:36 -0300

    Version 2.4.0
    UX Update

--- scripts/eventos.js
+++ scripts/eventos.js
@@ -92,11 +92,13 @@ document.addEventListener('click', () => menu.close())
 
 export function excluirTarefa(listaTarefas, resposta){
     var indice = listaTarefas.indexOf(tarefaSelecionada)
-    listaTarefas.splice(indice, 1)
-    localStorage.setItem('bancoTarefas', JSON.stringify(listaTarefas))
-    resposta.innerText = ''
-    resposta.innerText = `Tarefa ${tarefaSelecionada.titulo} Excluída!!`
-    resposta.style.color = '#A81C07'
+    if(confirm(`Deseja mesmo excluir a tarefa: ${tarefaSelecionada.titulo}?`)){
+        listaTarefas.splice(indice, 1)
+        localStorage.setItem('bancoTarefas', JSON.stringify(listaTarefas))
+        resposta.innerText = ''
+        resposta.innerText = `Tarefa ${tarefaSelecionada.titulo} Excluída!!`
+        resposta.style.color = '#A81C07'
+    }
 }
 
 export function editarTarefa(listaTarefas, resposta){
